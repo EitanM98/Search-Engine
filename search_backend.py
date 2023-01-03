56ac7a8d757f06b8b83b00e1925c0358b61644c3
Eitan Markman <eitanmarkman8@gmail.com>
2023-01-03 11:39:49 +0200

    import inverted index

--- search_backend.py
+++ search_backend.py
@@ -1,4 +1,5 @@
 import pickle
+import inverted_index_gcp
 
 with open('anchor_index.pkl', 'rb') as f:
     anchor_index = pickle.load(f)
@@ -8,4 +9,6 @@ with open('body_index.pkl', 'rb') as f:
 
 with open('title_index.pkl', 'rb') as f:
     title_index = pickle.load(f)
+
+print(type(anchor_index))
 
